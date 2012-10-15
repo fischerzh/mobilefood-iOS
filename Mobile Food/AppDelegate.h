8a709c413f4dca67b132ab5d29466a75566a62b3
Lion User <User@Lions-Mac.local>
2012-10-15 23:46:11 +0200

    Initial Commit

---
+++ Mobile Food/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mobile Food
+//
+//  Created by Lion User on 15.10.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Mobile Food/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Mobile Food
+//
+//  Created by Lion User on 15.10.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
